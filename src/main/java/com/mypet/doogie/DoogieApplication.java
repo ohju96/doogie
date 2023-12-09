51e58f04e0eda583a8e80d03247da5a89ae7bf89
ohju96 <ohju96@naver.com>
2023-12-09 13:30:52 +0900

    set : 프로젝트 셋팅

---
+++ src/main/java/com/mypet/doogie/DoogieApplication.java
@@ -0,0 +1,13 @@
+package com.mypet.doogie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoogieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoogieApplication.class, args);
+    }
+
+}
